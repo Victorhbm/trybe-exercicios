b6d585e5fc4a8ed730c9b53972e07d6edf5f0f56
Victor <victor.hugofla@hotmail.com>
2021-08-26 15:52:19 -0300

    Exercicios de fixacao finalizados

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,38 @@
+// Exercício de fixação 01
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Exercício de fixação 02
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// Exercício de fixação 03
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push('Contato')
+
+console.log(menu);
+
+// Exercício de fixação 04
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+	console.log(groceryList[index]);
+}
+
+// Exercício de fixação 05
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let name of names) {
+	console.log(name);
+}
+
