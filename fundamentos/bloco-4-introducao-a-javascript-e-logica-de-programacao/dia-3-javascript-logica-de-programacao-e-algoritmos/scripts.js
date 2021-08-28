9225f726337622d9cc25ab103e089206b07e9004
Victor <victor.hugofla@hotmail.com>
2021-08-28 10:50:17 -0300

    Exercicios 3 ao 4 finalizados

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -14,7 +14,7 @@ for (index = 0; index < n; index += 1) {
 
 // Exercicio 02 - Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
-let numero = 9;
+let numero = 5;
 let b = '';
 
 for (let i = 0; i < numero; i += 1) {
@@ -23,3 +23,48 @@ for (let i = 0; i < numero; i += 1) {
 }
 
 // Exercicio 03 - Agora inverta o lado do triângulo.
+
+let number = 5;
+let armazena = '';
+let position = number;
+
+for (let i = 0; i < number; i += 1) {
+	for (let columnIndex = 0; columnIndex <= number; columnIndex += 1) {
+		if (columnIndex < position) {
+			armazena += ' ';
+		} else {
+			armazena += '*';
+		}
+	}
+	
+	console.log(armazena);
+	armazena = '';
+	position -= 1;
+}
+
+// Exercicio 04 - Depois, faça uma pirâmide com n asteriscos de base:
+
+let number = 11;
+let armazena = '';
+let position = number;
+let meioDaMatriz = (number + 1) / 2;
+let controleEsquerdo = meioDaMatriz;
+let controleDireito = meioDaMatriz;
+
+for (let i = 0; i <= meioDaMatriz; i += 1) {
+	for (let columnIndex = 0; columnIndex <= number; columnIndex += 1) {
+		if (columnIndex > controleEsquerdo && columnIndex < controleDireito) {
+			armazena += '*';
+		} else {
+			armazena += ' ';
+		}
+	}
+	
+	console.log(armazena);
+	armazena = '';
+	controleDireito += 1;
+	controleEsquerdo -= 1; 
+}
+
+// Exercicio 05 - Faça uma pirâmide com n asteriscos de base, que seja vazia no meio:
+
