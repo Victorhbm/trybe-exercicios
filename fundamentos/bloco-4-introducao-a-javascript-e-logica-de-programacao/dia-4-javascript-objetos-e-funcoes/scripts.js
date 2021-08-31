d9a1554b754e98466e86d9c1811f94c945bf4110
Victor <victor.hugofla@hotmail.com>
2021-08-31 10:20:15 -0300

    Finaliza Exercicio 1 - Parte II

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -2,17 +2,17 @@
 
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 
-let info = {
-	personagem: 'Margarida',
-	origem: 'Pato Donald',
-	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+// 	personagem: 'Margarida',
+// 	origem: 'Pato Donald',
+// 	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
 //   console.log('Bem-vinda, ' + info.personagem);
 
 // 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
 
-info.recorrente = 'Sim';
+// info.recorrente = 'Sim';
 // console.log(info);
 
 // 3 - Faça um for/in que mostre todas as chaves do objeto.
@@ -29,17 +29,33 @@ info.recorrente = 'Sim';
 
 // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. 
 
-let newInfo = {
-	personagem: 'Tio Patinhas',
-	origem: 'Christmas on Bear Mountain, Dells Four Color Comics 178',
-	nota: 'O último MacPatinhas',
-	recorrente: 'Sim'
-}
+// let newInfo = {
+// 	personagem: 'Tio Patinhas',
+// 	origem: 'Christmas on Bear Mountain, Dells Four Color Comics 178',
+// 	nota: 'O último MacPatinhas',
+// 	recorrente: 'Sim'
+// }
 
-for (let index in info) {
-	if (info[index] === 'Sim' && newInfo[index] === 'Sim') {
-		console.log('Ambos recorrentes');
+// for (let index in info) {
+// 	if (info[index] === 'Sim' && newInfo[index] === 'Sim') {
+// 		console.log('Ambos recorrentes');
+// 	} else {
+// 		console.log(info[index] + ' e ' + newInfo[index]);
+// 	}
+// }
+
+// Parte II - Funções
+
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+function verificaPalindromo (palavra) {
+	let reverse = palavra.split('').reverse().join('');
+	if (reverse == palavra) {
+		return true
 	} else {
-		console.log(info[index] + ' e ' + newInfo[index]);
+		return false
 	}
 }
+
+console.log(verificaPalindromo('arara'));
+console.log(verificaPalindromo('desenvolvimento'));
