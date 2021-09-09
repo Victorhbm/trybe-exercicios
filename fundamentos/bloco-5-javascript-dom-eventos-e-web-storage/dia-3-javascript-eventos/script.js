7f53b6858efed1956d8bf39747401308b15257ff
Victor <victor.hugofla@hotmail.com>
2021-09-09 11:22:42 -0300

    Finaliza Exercicio 3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+// Exercicio 1
 function addDays () {
 	const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 	const daysList = document.getElementById('days');
@@ -21,17 +21,21 @@ function addDays () {
 		const dayListItem = document.createElement('li')
 		dayListItem.innerHTML = dezDaysList[i];
 		dayListItem.className = 'day'
-		if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31) {
+		if (dezDaysList[i] === 24 || dezDaysList[i] === 31) {
 			dayListItem.className = 'day holiday';
 		}
-		if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 || dezDaysList[i] === 25) {
+		if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18) {
 			dayListItem.className = 'day friday';
+		}
+		if (dezDaysList[i] === 25) {
+			dayListItem.className = 'day friday holiday';
 		}
 		daysList.appendChild(dayListItem)
 	}
 }
 addDays();
 
+// Exercicio 2
 function makeButton (feriado) {
 	const button = document.createElement('button');
 	button.innerHTML = feriado;
@@ -40,3 +44,15 @@ function makeButton (feriado) {
 }
 makeButton('Feriados');
 
+// Exercicio 3
+holidayButton = document.getElementById('btn-holiday');
+holiday = document.querySelectorAll('.holiday');
+	for (let index = 0; index < holiday.length; index += 1) {
+		holidayButton.addEventListener('click', function(){
+			if (holiday[index].style.backgroundColor === 'green') {
+				holiday[index].style.backgroundColor = 'rgb(238,238,238)';
+			} else {
+				holiday[index].style.backgroundColor = 'green';
+			}
+		})
+	}
