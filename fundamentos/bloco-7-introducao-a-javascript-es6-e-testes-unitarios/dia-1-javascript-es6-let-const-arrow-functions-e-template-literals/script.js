d2feab0ee07bbe78685041ab7d200c4d513f641b
Victor <victor.hugofla@hotmail.com>
2021-09-23 18:58:17 -0300

    Finaliza exercício 1 e 2 - parte 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,4 +1,4 @@
-/* Exercício 1 */
+/* Parte I - Exercício 1 */
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -14,5 +14,26 @@ testingScope(true);
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Exercício 2
+// Parte I - Exercício 2
 console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
+
+// Parte II - Exercício 1
+const calcFatorial = (n) => {
+  let result = 1;
+  for (let i = 1; i <= n; i += 1) {
+    result *= i;
+  }
+  console.log(result);
+}
+calcFatorial(4);
+
+// Parte II - Exercício 2
+const longestWord = (frase) => {
+  let separaFrase = frase.split(' ');
+  let winner = '';
+  for (let i = 0; i < separaFrase.length; i += 1) {
+    (separaFrase[i].length > winner.length) ? winner = separaFrase[i] : winner;
+  }
+  console.log(winner);
+}
+longestWord('Antônio foi no banheiro e não sabemos o que aconteceu');
