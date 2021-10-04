f5d64c6170a13ea99e8bd597cef60c05ff40726b
Victor <victor.hugofla@hotmail.com>
2021-10-04 18:34:51 -0300

    Finaliza exercicio bonus parte I

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -61,4 +61,3 @@ const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 console.log(resultadoDaProva(RIGHT_ANSWERS, STUDENT_ANSWERS, checkGabarito));
-
