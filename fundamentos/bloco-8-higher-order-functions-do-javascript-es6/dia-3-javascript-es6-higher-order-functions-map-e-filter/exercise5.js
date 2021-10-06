d1143b6f6d6684667fffc84dcb5c356ea4b2b090
Victor <victor.hugofla@hotmail.com>
2021-10-06 19:06:56 -0300

    Finaliza exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise5.js
@@ -62,3 +62,21 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+// 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors() {
+  const filteredGenre = books.filter((book) => {
+    return book.genre === 'Ficção Científica' || book.genre === 'Fantasia'
+  });
+  const getNamers = filteredGenre.map((book) => `${book.author.name}`);
+
+  return getNamers.sort();
+}
+
+//console.log(fantasyOrScienceFictionAuthors());
