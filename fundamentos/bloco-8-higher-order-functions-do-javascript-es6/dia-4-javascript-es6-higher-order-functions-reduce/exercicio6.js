cd48ed78872065a98b68683c7cebd8135014bd27
Victor <victor.hugofla@hotmail.com>
2021-10-08 10:38:48 -0300

    Finaliza exercicio 6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
@@ -4,8 +4,17 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.map((student, index) => {
+    return {
+      name: student,
+      average: (grades[index].reduce((acc, currentValue) => {
+        return acc + currentValue
+      }) / grades[index].length)
+    }
+  })
 }
+
+console.log(studentAverage());
 
 const expected = [
   { name: 'Pedro Henrique', average: 7.8 },
