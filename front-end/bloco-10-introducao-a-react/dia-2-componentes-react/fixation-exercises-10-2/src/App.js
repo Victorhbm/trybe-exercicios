a79b958b1cac7ccccfd7f65f0a00e533af1141f9
Victor <victor.hugofla@hotmail.com>
2021-10-28 15:40:10 -0300

    Finaliza exercicio de fixação 2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
-import Image from './Image.js'
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
 import './App.css';
+import Order from './Order';
 
-function App() {
-  return (
-    <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alt="Cute cat staring"/>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div>
+        <h1> Orders recently created </h1>
+        <Order order={headphone}/>
+        <Order order={energyDrink}/>
+      </div>
+    );
+  }
 }
 
 export default App;
