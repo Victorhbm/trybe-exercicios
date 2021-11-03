60510a577f32341d3c4d1db43c49def8203a80ed
Victor <victor.hugofla@hotmail.com>
2021-11-03 15:44:41 -0300

    Finaliza exercicio para-fixar 2

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,35 @@
 import './App.css';
 import React from 'react';
 
-function handleClickVictor() {
-  console.log('Victor');
-}
 
-function handleClickHugo() {
-  console.log('Hugo');
-}
-
-function handleClickBezerra() {
-  console.log('Bezerra');
-}
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.handleClickVictor = this.handleClickVictor.bind(this);
+    this.handleClickHugo = this.handleClickHugo.bind(this);
+    this.handleClickBezerra = this.handleClickBezerra.bind(this);
+  }
+
+  handleClickVictor() {
+    console.log('Victor');
+  }
+  
+  handleClickHugo() {
+    console.log('Hugo');
+  }
+  
+  handleClickBezerra() {
+    console.log('Bezerra');
+  }
+
   render() {
     return (
       <div>
-        <button onClick={handleClickVictor}>Clique aqui!</button>
-        <button onClick={handleClickHugo}>Clique aqui!</button>
-        <button onClick={handleClickBezerra}>Clique aqui!</button>
+        <button onClick={this.handleClickVictor}>Clique aqui!</button>
+        <button onClick={this.handleClickHugo}>Clique aqui!</button>
+        <button onClick={this.handleClickBezerra}>Clique aqui!</button>
       </div>
     );
   }
