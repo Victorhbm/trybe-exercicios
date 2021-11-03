23a03b19d59e1f610b79934154d4620cf37b0fb7
Victor <victor.hugofla@hotmail.com>
2021-11-03 16:04:50 -0300

    Finaliza exercicio para-fixar 3

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -7,31 +7,43 @@ class App extends React.Component {
   constructor() {
     super()
 
-    this.handleClickVictor = this.handleClickVictor.bind(this);
-    this.handleClickHugo = this.handleClickHugo.bind(this);
-    this.handleClickBezerra = this.handleClickBezerra.bind(this);
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0
+    }
+
+    this.handleClick1 = this.handleClick1.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
   }
 
-  handleClickVictor() {
-    console.log('Victor');
+  handleClick1() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
+    }));
   }
   
-  handleClickHugo() {
-    console.log('Hugo');
+  handleClick2() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1
+    }));
   }
   
-  handleClickBezerra() {
-    console.log('Bezerra');
+  handleClick3() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1
+    }));
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClickVictor}>Clique aqui!</button>
-        <button onClick={this.handleClickHugo}>Clique aqui!</button>
-        <button onClick={this.handleClickBezerra}>Clique aqui!</button>
+        <button onClick={this.handleClick1}>{this.state.numeroDeCliques1}</button>
+        <button onClick={this.handleClick2}>{this.state.numeroDeCliques2}</button>
+        <button onClick={this.handleClick3}>{this.state.numeroDeCliques3}</button>
       </div>
-    );
+    )
   }
 }
 
